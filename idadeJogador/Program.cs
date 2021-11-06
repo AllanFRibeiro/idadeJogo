f1fdfd48766035e0c70ca4ca6d7855cf9687cff5
Allan F Ribeiro <allanfribeiro@gmail.com>
2021-11-06 13:36:39 -0300

    Idade para Jogar

---
+++ idadeJogador/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace idadeJogador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte idade;
+
+            Console.WriteLine("Digite a idade do jogador: ");
+            idade = Convert.ToByte(Console.ReadLine());
+
+            if (idade >= 12)
+            {
+                Console.WriteLine("Você pode jogar!");
+
+            }
+            else if (idade < 12)
+            {
+
+                Console.WriteLine("Você não tem idade para jogar");
+            }
+            else
+            {
+
+                Console.WriteLine("Por favor digite somente números");
+            }
+
+        }
+    }
+}
